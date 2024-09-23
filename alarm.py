99d9891538a60f0c22c02993e8507ce401773318
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-23 13:48:47 +0100

    Created alarm.py

---
+++ alarm.py
@@ -0,0 +1,13 @@
+# Alarm Clock
+
+import time
+import datetime
+import pygame
+
+def set_alarm(alarm_time):
+  print(f"Alarm sset for {alarm_time}")
+
+if __name__ == "__main__":
+  alarm_time = input("Enter the alarm time (HH:MM:SS): ")
+  set_alarm(alarm_time)
+
